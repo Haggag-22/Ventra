a2cab17ff8b678039aca1adc65d739f5d081e193
Omar Haggag <omhg22@gmail.com>
2025-11-13 09:37:37 -0500

    AWS

---
+++ collector/cloudtrail/cloudtrail_s3.py
@@ -0,0 +1,4 @@
+def run_cloudtrail_s3(args):
+    print("[Collector] CloudTrail S3 Raw Logs")
+    print(f"Bucket: {args.bucket}, Prefix: {args.prefix}, Region: {args.region}")
+    # TODO: Implement real collector
