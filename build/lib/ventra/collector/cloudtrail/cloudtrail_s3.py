ea73cdb0efc2a65b4f4ef13447993351929f9808
Haggag-22 <omhg22@gmail.com>
2025-11-13 16:34:52 -0500

    Remove build artifacts from repo

--- build/lib/ventra/collector/cloudtrail/cloudtrail_s3.py
+++
@@ -1,4 +0,0 @@
-def run_cloudtrail_s3(args):
-    print("[Collector] CloudTrail S3 Raw Logs")
-    print(f"Bucket: {args.bucket}, Prefix: {args.prefix}, Region: {args.region}")
-    # TODO: Implement real collector
